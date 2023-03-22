459c593904b800daad8d7399451da8c386c009bc
John U <arjorb@gmail.com>
2023-03-22 17:45:38 +0200

    updating the submit button

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -23,7 +23,7 @@ const TodoInput = () => {
           placeholder='add todo...'
           className='w-full bg-transparent text-lg border-none outline-none placeholder:text-gray-200'
         />
-        <HiPlusCircle onClick={handleSubmit} size={30} className={`cursor-pointer ${input.trim() === '' ? 'text-gray-300 cursor-not-allowed' : 'text-green-500'} duration-500`} />
+        <HiPlusCircle onClick={handleSubmit} size={30} className={`cursor-pointer ${input.trim() === '' ? 'text-gray-300' : 'text-green-500'} duration-700`} />
       </div>
     </div>
   );
