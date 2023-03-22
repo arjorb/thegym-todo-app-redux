a823f026150b6bf165c075eccd24d0e9c0d83f4a
John U <arjorb@gmail.com>
2023-03-22 17:39:21 +0200

    done implementing the action function to updateTodo

--- src/features/todo.js
+++ src/features/todo.js
@@ -7,13 +7,23 @@ export const todoSlice = createSlice({
   initialState: { value: initialState },
   reducers: {
     addTodo: (state, action) => {
-      state.value.push({ id: nanoid(), title: action.payload, isChecked: false });
+      state.value.push({ id: nanoid(), title: action.payload, isChecked: false, isEdited: false });
     },
 
     removeTodo: (state, action) => {
       state.value = state.value.filter(prev => prev.id !== action.payload);
     },
-    updateTodo: (state, action) => {},
+    updateTodo: (state, action) => {
+      state.value = state.value.map(prev => {
+        return prev.id === action.payload.id && prev.isEdited ? { ...prev, title: action.payload.value } : prev;
+      });
+    },
+
+    editTodo: (state, action) => {
+      state.value = state.value.map(prev => {
+        return prev.id === action.payload ? { ...prev, isEdited: !prev.isEdited } : prev;
+      });
+    },
     completedTodo: (state, action) => {
       state.value = state.value.map(prev => {
         if (prev.id === action.payload) {
@@ -24,5 +34,5 @@ export const todoSlice = createSlice({
   },
 });
 
-export const { addTodo, removeTodo, updateTodo, completedTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo, editTodo, completedTodo } = todoSlice.actions;
 export default todoSlice.reducer;
